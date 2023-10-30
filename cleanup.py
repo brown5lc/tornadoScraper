1700a284cc5dc9f3e508d5de3f018ea913f86bac
brown5lc <liamxxbrown@gmail.com>
2023-10-30 14:35:22 -0400

    modified cleanup.py to incorporate the radar station data and add the nearest radar station's code to each tornadic event. This data is now being stored in enriched_tornado_data.csv which will probably be used to query aws.

--- cleanup.py
+++ cleanup.py
@@ -1,22 +1,28 @@
 import pandas as pd
+from scipy.spatial import KDTree
 
-# Load NOAA CSV data
-data = pd.read_csv('./1950-2022_all_tornadoes')
-radar_df = pd.read_csv('./radar_locations.csv')
+# Load the radar station data
+radar_df = pd.read_csv('radar_locations.csv')
+radar_points = list(zip(radar_df['Latitude'], radar_df['Longitude']))
 
-# Filter out tornadoes before 1990
-filteredData = data[data['yr'] >= 1990]
+# Create a k-d tree for the radar stations
+kdtree = KDTree(radar_points)
 
-# Function to determine the closest radar sire based on tornado latitude and longitude
-def getClosestRadarSite(lat, long):
-    # TODO: impliment the logic to find the closest radar site
-    # I'll need a list of all the radar sites with the lat and long, preferabbly sorted so I can binary search
-    # return the four letter station identifier
-    
-    pass
+# Load tornado data
+tornado_df = pd.read_csv('1950-2022_all_tornadoes.csv')
 
-# Apply the function to each tornado event
-filteredData['radar_site'] = filteredData.apply(lambda x: getClosestRadarSite(x['slat'], x['slon']), axis=1)
+# Filter out tornado data before a certain year, e.g., 2000
+filtered_data = tornado_df[tornado_df['yr'] >= 2010]
 
-# Save the data to a new CSV file
-filteredData.to_csv('./processedTornadoData', index=False)
+# For each tornado location in the filtered_data, find the nearest radar station
+nearest_radars = []
+for idx, row in filtered_data.iterrows():
+    _, index = kdtree.query((row['slat'], row['slon']))
+    nearest_radar = radar_df.iloc[index]
+    nearest_radars.append(nearest_radar['RadarCode'])
+
+# Add the nearest radar station ID to the filtered_data DataFrame
+filtered_data['Nearest Radar Station'] = nearest_radars
+
+# Save the enriched filtered_data to a new CSV
+filtered_data.to_csv('enriched_tornado_data.csv', index=False)
