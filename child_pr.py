27a974ac8e6e54854675a2b3f56b3cdcc3e14373
brown5lc <liamxxbrown@gmail.com>
2023-11-16 12:45:59 -0500

    Changed the way it's downloading and visualizing the data to keep everything in memory rather than saving it to the local file-system because Vercel doesn't have a local filesystem

--- child_pr.py
+++ child_pr.py
@@ -1,5 +1,6 @@
 # Importing necessary libraries
 import os
+import io
 import sys
 import gzip
 import shutil
@@ -53,43 +54,37 @@ def find_most_recent_file(bucket_name, radar_site):
     return files[-1]
 
 # Download the most recent radar file for the provided radar site
-def download_and_decompress(filename): 
-    # Download the file from S3 and decompress it if necessary
-
-    actual_file = os.path.basename(filename)
-    compressed_path = os.path.join(compressed_dir, actual_file)
+def download_and_decompress(filename):
     s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+
+    # Using BytesIO to handle the file content in memory
+    compressed_file_obj = io.BytesIO()
+
     try:
-        # Download the file
-        s3.download_file(bucket_name, filename, compressed_path)
-        
-        # If the file is a gzip, uncompress it and store in uncompressed_dir
-        if actual_file.endswith(".gz"):
-            uncompressed_file = actual_file.rstrip('.gz')
-            uncompressed_path = os.path.join(uncompressed_dir, uncompressed_file)
-            
-            with gzip.open(compressed_path, 'rb') as f_in:
-                with open(uncompressed_path, 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
-            os.remove(compressed_path)  # Remove the gz file after decompressing
+        # Download the file directly into memory
+        s3.download_fileobj(bucket_name, filename, compressed_file_obj)
+
+        # If the file is a gzip, decompress it
+        if filename.endswith(".gz"):
+            compressed_file_obj.seek(0)  # Go to the beginning of the file
+            with gzip.GzipFile(fileobj=compressed_file_obj, mode='rb') as f_in:
+                decompressed_data = io.BytesIO(f_in.read())
+            return decompressed_data
         else:
-            # If not a gzip, simply move it to the uncompressed_dir
-            shutil.move(compressed_path, os.path.join(uncompressed_dir, actual_file))
+            # If not a gzip, simply return the BytesIO object
+            compressed_file_obj.seek(0)  # Go to the beginning of the file
+            return compressed_file_obj
 
     except Exception as e:
         print(f"Failed to download and process file {filename}. Error: {e}")
-
-    return 
+        return None
 
 
 # Visualizing the radar data
 def visualize_radar_data(filename, user_lat, user_lon):
 
-    # Extract the filename from the path
-    acutal_file = os.path.basename(filename)
-
     # Read in the file
-    radar = pyart.io.read_nexrad_archive(f'./uncompressed_files/{acutal_file}')
+    radar = pyart.io.read_nexrad_archive(filename)
 
     # Create a plot
     display = pyart.graph.RadarDisplay(radar)
@@ -109,12 +104,8 @@ def visualize_radar_data(filename, user_lat, user_lon):
     display.plot_range_ring(radar.range['data'][-1]/1000., ax=ax)
     display.set_limits(xlim=(-150, 150), ylim=(-150, 150), ax=ax)
 
-
     # Show the plot
     plt.show()
-
-    # Remove the uncompressed file
-    os.remove(f'./uncompressed_files/{os.path.basename(filename)}')
 
     return
 
@@ -129,13 +120,13 @@ def main(lat, lon):
 
     prefix = f'{now.year}/{now.month:02}/{now.day:02}/{radar_site}'
 
-    filename= find_most_recent_file(bucket_name, prefix)
+    filename = find_most_recent_file(bucket_name, prefix)
 
     # Download the radar file
-    download_and_decompress(filename)
+    decompressed_data = download_and_decompress(filename)
 
     # Visualize the radar file
-    visualize_radar_data(filename, lat, lon)
+    visualize_radar_data(decompressed_data, lat, lon)
  
     return 
 """
